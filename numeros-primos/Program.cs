912d1a8e725214ccd89f6ba13052d544bfb32e53
brendaandrade09 <brenda.andrade@edu.senai.br>
2026-04-08 20:59:38 -0300

    Add files via upload

---
+++ numeros-primos/Program.cs
@@ -0,0 +1,23 @@
+﻿//Feito por Brenda Lima Andrade
+Console.BackgroundColor = ConsoleColor.DarkMagenta;
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Descobrindo números primos!!! Digite até qual número deseja descobrir os números primos: ");
+int.TryParse(Console.ReadLine(), out int nm);
+
+
+for (int i = 2; i <= nm; i++)
+{
+    int div = 0;
+    for (int j = 1; j <= i; j++)
+    {
+        if (i % j == 0)
+        {
+            div++;
+        }
+    }
+    if (div == 2)
+    {
+        Console.WriteLine(i);
+    }
+}
+
