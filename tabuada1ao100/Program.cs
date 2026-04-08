59fbe7ff02d2eb63d70ad3a3cfb1481f9884fec5
brendaandrade09 <brenda.andrade@edu.senai.br>
2026-04-08 20:53:55 -0300

    Tabuada do 1 ao 100 por Brenda Lima Andrade

---
+++ tabuada1ao100/Program.cs
@@ -0,0 +1,11 @@
+﻿//Feito por Brenda Lima Andrade
+Console.BackgroundColor = ConsoleColor.DarkMagenta;
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.Write("Vamos ver a tabuada do 1 ao 10!!\n");
+for (int i = 1; i <= 10; i++)
+{
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"Tabuada do {i}: {i}x{j}= {i * j}");
+    }
+}
