da79417754345d2cc20da0c6456ae72d7e715df8
brendaandrade09 <brenda.andrade@edu.senai.br>
2026-04-08 21:16:11 -0300

    Add files via upload

---
+++ area/Program.cs
@@ -0,0 +1,10 @@
+﻿//Feito por Brenda Lima Andrade
+Console.BackgroundColor = ConsoleColor.DarkMagenta;
+Console.ForegroundColor = ConsoleColor.Cyan;
+for (int i = 0; i < 3; i++)
+{
+    Console.WriteLine("Digite o valor do raio:");
+    float.TryParse(Console.ReadLine(), out float raio);
+    float area = (float)(Math.Pow(raio, 2.0) * (float)Math.PI);
+    Console.WriteLine($"A área da circunferência é: {area} cm²");
+}
