efc2eb539edc4a862a6c6c5d64869947c87701aa
brendaandrade09 <brenda.andrade@edu.senai.br>
2026-04-08 21:24:24 -0300

    Add files via upload

---
+++ progressao-aritmetica/Program.cs
@@ -0,0 +1,13 @@
+﻿//Feito por Brenda Lima Andrade
+Console.WriteLine("Calculadora de PA!! Digite a quantidade de termos:");
+int.TryParse(Console.ReadLine()!, out int termos);
+Console.WriteLine("Digite a razão:");
+int.TryParse(Console.ReadLine()!, out int razao);
+Console.WriteLine("\n \n A sequência é:");
+for (int i = 1; i <= termos; i++)
+{
+    int result = i * razao;
+    Console.Write($"{result}, ");
+}
+Console.Write("...");
+
