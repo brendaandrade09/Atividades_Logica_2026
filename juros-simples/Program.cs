dc9cc0de13bfe15f30fee54f304e86b13005e67f
brendaandrade09 <brenda.andrade@edu.senai.br>
2026-04-08 21:06:24 -0300

    Add files via upload

---
+++ juros-simples/Program.cs
@@ -0,0 +1,9 @@
+﻿//Feito por Brenda Lima Andrade
+Console.WriteLine("Calculadora de juros simples!!! Primeiramente, informe seu capital: ");
+float.TryParse(Console.ReadLine(), out float c);
+Console.Write("Agora, informe a taxa dos juros:");
+float.TryParse(Console.ReadLine(), out float tax);
+Console.WriteLine("Informe o tempo em meses:");
+float.TryParse(Console.ReadLine(), out float t);
+float jurosSimples = c * t / 100 * tax;
+Console.WriteLine($"Seus juros simples são: R${jurosSimples}.");
