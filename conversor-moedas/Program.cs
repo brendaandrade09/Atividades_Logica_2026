9ea265ea178011d79c30ee9a2fd1ab5a0aecc1cc
brendaandrade09 <brenda.andrade@edu.senai.br>
2026-04-08 21:22:12 -0300

    Add files via upload

---
+++ conversor-moedas/Program.cs
@@ -0,0 +1,28 @@
+﻿//Feito por Brenda Lima Andrade
+Console.WriteLine("\n \n Conversor de moedas!! \n Digite o valor que você quer converter: ");
+float moeda, conversorReal, resultado;
+int escolha;
+float.TryParse(Console.ReadLine()!, out moeda);
+Console.WriteLine("\n Digite 1 para converter para dólar; \n Digite 2 para converter para libras.");
+int.TryParse(Console.ReadLine()!, out escolha);
+switch (escolha)
+{
+    case 1:
+        Console.WriteLine("Digite o valor do dólar turismo:");
+        float.TryParse(Console.ReadLine()!.Replace(".", ","), out
+conversorReal);
+        resultado = moeda * conversorReal;
+        Console.WriteLine($"A conversão é: US${resultado}");
+        break;
+    case 2:
+        Console.WriteLine("Digite o valor da libra");
+        float.TryParse(Console.ReadLine()!.Replace(".", ","), out
+conversorReal);
+        resultado = moeda * conversorReal;
+        Console.WriteLine($"A conversão é: £{resultado}");
+        break;
+    default:
+        Console.WriteLine("Digite um valor válido!!");
+        break;
+}
+
