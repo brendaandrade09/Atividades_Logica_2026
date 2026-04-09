7fdb9bf327c69754bb594ec8fe1de9c39a935e7a
brendaandrade09 <brenda.andrade@edu.senai.br>
2026-04-09 13:29:47 -0300

    Add files via upload

---
+++ raiz-quadrada/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.BackgroundColor = ConsoleColor.DarkMagenta;
+Console.ForegroundColor = ConsoleColor.Cyan;
+//Feito por Brenda Lima Andrade
+Console.Write("Calculadora de raiz quadrada!! Digite o número que deseja descobrir a raiz quadrada:");
+double.TryParse(Console.ReadLine()!.Replace(".", ","), out double raiz);
+double result = Math.Pow(raiz, 0.5);
+Console.WriteLine($"A raiz quadrada de {raiz} é {result}.");
